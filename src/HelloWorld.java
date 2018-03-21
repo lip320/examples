9d7e493ac132316b55991872860ce34bdd45400b
lip320 <l-ip320@hotmail.com>
2018-03-21 14:25:11 +0000

    update on pathing and naming

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+package examples;
+
+/**
+ * Hello world!
+ */
+public class HelloWorld
+{
+
+    private final String message = "Hello World!";
+
+    public HelloWorld() {}
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
