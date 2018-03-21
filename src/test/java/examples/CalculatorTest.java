49751ac46a7683574a9ddd2ff6d0072c98560e1d
lip320 <l-ip320@hotmail.com>
2018-03-21 15:49:11 +0000

    re Cal***Test

--- src/test/java/examples/CalculatorTest.java
+++ src/test/java/examples/CalculatorTest.java
@@ -5,7 +5,7 @@ public class CalculatorTest {
   @Test
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
-    int sum = calculator.evaluate("1+12+3");
+    int sum = calculator.evaluate("1+2+3");
     assertEquals(6, sum);
   }
 }
